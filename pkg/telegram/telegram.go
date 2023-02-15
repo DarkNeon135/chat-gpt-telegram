b1e6ea1ddde02686dda0ac60aba2b0b3cb50811a
Oleh Lysenko <baryn72@gmail.com>
2023-02-15 19:18:33 +0100

    - added request manager for Telegram chat

--- pkg/telegram/telegram.go
+++ pkg/telegram/telegram.go
@@ -3,15 +3,28 @@ package telegram
 import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"net/http"
 	"open-gpt-telegram/internal/log"
 	"open-gpt-telegram/pkg/db"
+	"open-gpt-telegram/pkg/openAI"
+	"strings"
+	"sync"
 	"time"
 )
 
 type Telegram struct {
 	MongoConnector *db.MongoConnector
-	updated        bool
 	BotApi         *tgbotapi.BotAPI
+	OpenAIKey      string
+	usersList      map[int64]telegramUser
+}
+
+type telegramUser struct {
+	lastMessageTime time.Time
+	isAllowed       bool
+	requestCounter  int
+	client          http.Client
+	isUserNotified  bool
 }
 
 func ConnectToTelegram(apiToken string) (*tgbotapi.BotAPI, error) {
@@ -25,62 +38,62 @@ func ConnectToTelegram(apiToken string) (*tgbotapi.BotAPI, error) {
 	return botApi, nil
 }
 
-func (telegram *Telegram) CheckUsersStatus() error {
+func (telegram *Telegram) CheckUsersStatus() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := telegram.BotApi.GetUpdatesChan(u)
 
-	usersList := make(map[int64]telegramUser)
+	telegram.usersList = make(map[int64]telegramUser)
 
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-		chatId := update.Message.Chat.ID
 
-		_, isExist := usersList[chatId]
-
-		if !isExist {
-			usersList[chatId] = telegramUser{
-				lastMessageTime: time.Now(),
-				isAllowed:       true,
-				requestCounter:  1,
-			}
-		} else {
-			usersList[chatId] = checkRequestsFrequency(usersList[chatId])
-
-			if !usersList[chatId].isAllowed {
-				if !usersList[chatId].isUserNotified {
-					_, err := telegram.BotApi.Send(tgbotapi.NewMessage(chatId, "You reached request limit, wait a minute to continue."+
-						"\nAll your messages will be ignored!"))
-					if err != nil {
-						return fmt.Errorf("sending message to chat failed. Error: %s", err)
-					}
-					usersList[chatId] = telegramUser{
-						lastMessageTime: usersList[chatId].lastMessageTime,
-						requestCounter:  usersList[chatId].requestCounter,
-						isAllowed:       true,
-						isUserNotified:  true,
-					}
-				}
-				continue
-			}
-		}
-
-		if update.Message.IsCommand() {
-			err := telegram.checkTelegramCommand(update, chatId)
-			if err != nil {
-				return err
-			}
-
-		} else {
-			//TODO: Call OpenAI func
-
-		}
+		go telegram.handleUserMessage(update)
 
 	}
-	return nil
+}
+
+func (telegram *Telegram) handleUserMessage(update tgbotapi.Update) {
+
+	if update.Message == nil {
+		return
+	}
+	chatId := update.Message.Chat.ID
+
+	if update.Message.IsCommand() {
+		err := telegram.checkTelegramCommand(update, chatId)
+		if err != nil {
+			log.Error.Fatal(err)
+		}
+		return
+	}
+
+	_, isExist := telegram.usersList[chatId]
+
+	isOk, err := telegram.processTelegramMessage(isExist, chatId, update)
+	if err != nil {
+		log.Error.Fatal(err)
+	}
+	if !isOk {
+		return
+	}
+
+	gptAnswer, err := openAI.SendRequestToOpenAI(telegram.OpenAIKey, update.Message.Text, telegram.usersList[chatId].client)
+	if err != nil && strings.HasSuffix(err.Error(), "context deadline exceeded") {
+		_, err = telegram.BotApi.Send(tgbotapi.NewMessage(chatId, "OpenAI under the pressure, please try again in a several minutes."))
+		if err != nil {
+			log.Error.Fatal(fmt.Errorf("sending message to chat failed. Error: %s", err))
+		}
+		return
+
+	} else if err != nil {
+		log.Warning.Println(err)
+	}
+
+	_, err = telegram.BotApi.Send(tgbotapi.NewMessage(chatId, gptAnswer))
+	if err != nil {
+		log.Error.Fatal(fmt.Errorf("sending message to chat failed. Error: %s", err))
+	}
 }
 
 func (telegram *Telegram) SendMessagesToChannel(message string) error {
@@ -149,9 +162,82 @@ func checkRequestsFrequency(userInfo telegramUser) telegramUser {
 	return userInfo
 }
 
-type telegramUser struct {
-	lastMessageTime time.Time
-	isAllowed       bool
-	requestCounter  int
-	isUserNotified  bool
+func (telegram *Telegram) checkAllowanceToChat(chatId int64, mut *sync.Mutex) bool {
+
+	if !telegram.usersList[chatId].isAllowed {
+		if !telegram.usersList[chatId].isUserNotified {
+			_, err := telegram.BotApi.Send(tgbotapi.NewMessage(chatId, "You reached request limit, wait a minute to continue."+
+				"\nAll your messages will be ignored!"))
+			if err != nil {
+				log.Error.Fatal(fmt.Errorf("sending message to chat failed. Error: %s", err))
+			}
+			mut.Lock()
+			telegram.usersList[chatId] = telegramUser{
+				lastMessageTime: telegram.usersList[chatId].lastMessageTime,
+				requestCounter:  telegram.usersList[chatId].requestCounter,
+				isAllowed:       true,
+				isUserNotified:  true,
+				client:          telegram.usersList[chatId].client,
+			}
+			mut.Unlock()
+		}
+		return false
+	}
+	return true
 }
+
+func (telegram *Telegram) processTelegramMessage(isExist bool, chatId int64, update tgbotapi.Update) (bool, error) {
+	mut := sync.Mutex{}
+
+	if !isExist {
+		_, err := telegram.MongoConnector.CheckChatId(chatId) //checks user command /start
+		if err != nil {
+			return false, nil
+		}
+
+		mut.Lock()
+		telegram.usersList[chatId] = telegramUser{
+			lastMessageTime: time.Now(),
+			isAllowed:       true,
+			requestCounter:  1,
+			client:          http.Client{},
+		}
+		mut.Unlock()
+
+		if len(update.Message.Text) < 3 {
+			_, err = telegram.BotApi.Send(tgbotapi.NewMessage(chatId, "Please, send message more than 3 chars."))
+			if err != nil {
+				return false, fmt.Errorf("sending message to chat failed. Error: %s", err)
+			}
+			return false, nil
+		}
+
+	} else {
+		mut.Lock()
+		telegram.usersList[chatId] = checkRequestsFrequency(telegram.usersList[chatId])
+		mut.Unlock()
+
+		if !telegram.checkAllowanceToChat(chatId, &mut) {
+			return false, nil
+		}
+
+		if len(update.Message.Text) < 3 {
+			_, err := telegram.BotApi.Send(tgbotapi.NewMessage(chatId, "Send message more than 3 chars"))
+			if err != nil {
+				return false, fmt.Errorf("sending message to chat failed. Error: %s", err)
+			}
+			return false, nil
+		}
+
+	}
+	return true, nil
+}
+
+//func (telegram *Telegram) receivePreviousMessage(chatId int64) err {
+//	chat, err := telegram.BotApi.GetChat(tgbotapi.ChatInfoConfig{
+//		ChatConfig: tgbotapi.ChatConfig{ChatID: chatId},
+//	})
+//	if err != nil {
+//
+//	}
+//}
